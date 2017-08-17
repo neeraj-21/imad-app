8559f635b1f1cb92d01091758fb2ac7449f422e8
neeraj-21 <30729011+neeraj-21@users.noreply.github.com>
2017-08-17 20:52:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ button.onclick 	= 	function(){
 		
 	}
 
-	request.open('GET','http://http://neerajbhusari.imad.hasura-app.io//counter',true);
+	request.open('GET','http://http://neerajbhusari.imad.hasura-app.io/counter',true);
 	request.send(null);
 
 };
